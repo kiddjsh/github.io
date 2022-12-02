4f4bc54703c2c969e9c915007a2fffb7b33efe99
kiddjsh <joshua.kidder@gmail.com>
2022-12-02 02:10:14 -0500

    php services update

--- mail.php
+++ mail.php
@@ -1,16 +1,24 @@
 <?php
 
-$name =$_POST["name"];
-$email =$_POST["email"];
-$subject =$_POST["subject"];
-$message =$_POST["message"];
-
-$mailheader = "From:".$name."<".$email.">\r\n";
-
-$receipient = "development@kiddjsh.xyz";
-
-mail ($receipient, $subject, $message, $mailheader) 
-    or die("Error!");
+$mail_to_send_to = "development@kiddjsh.xyz";
+$from_email = "development@kiddjsh.xyz";
+$sendflag = $_REQUEST['sendflag'];    
+$name=$_REQUEST['name'];
+if ( $sendflag == "send" )
+        {
+                $subject= "Message subject";
+                $email = $_REQUEST['email'] ;
+                $message= "\r\n" . "Name: $name" . "\r\n"; //get recipient name in contact form
+                $message = $message.$_REQUEST['message'] . "\r\n" ;//add message from the contact form to existing message(name of the client)
+                $headers = "From: $from_email" . "\r\n" . "Reply-To: $email"  ;
+                $a = mail( $mail_to_send_to, $subject, $message, $headers );
+                if ($a)
+                {
+                     print("Message was sent, you can send another one");
+                } else {
+                     print("Message wasn't sent, please check that you have changed emails in the bottom");
+                }
+        }
 
 echo '
 
